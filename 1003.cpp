3eafc00d9f71253d830a7703a840f711fe9e4707
bengHak <41236155+bengHak@users.noreply.github.com>
2020-02-11 21:42:51 +0900

    9461

--- 1003.cpp
+++ 1003.cpp
@@ -1,34 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int zero, one;
-
-int fibonacci(int n){
-    if (n == 0){
-        zero ++;
+#include <iostream>
+ 
+int a[2] = { 0 };
+ 
+int fibonacci(int n) {
+    if (n == 0) {
+        ::a[0]++;
         return 0;
     }
-    else if(n == 1 ){
-        one ++;
+    else if (n == 1) {
+        ::a[1]++;
         return 1;
     }
-    else
-        return fibonacci(n-1) + fibonacci(n-2);
-}
-
-int main ()
-{
-    int T; //testCase
-    scanf("%d",&T);
-
-    for(int i = 0 ; i < T ;i++){
-        zero = 0; one =0;
-        int temp;
-
-        scanf("%d", &temp);
-        fibonacci(temp);
-        printf("%d %d\n",zero,one);
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
-
+}
+ 
+int main() {
+    int n;
+    scanf("%d", &n);
+ 
+    for (int i = 0; i < n; ++i) {
+        int m;
+        scanf("%d", &m);
+        fibonacci(m);
+ 
+        printf("%d %d\n", ::a[0], ::a[1]);
+        ::a[0] = 0;
+        ::a[1] = 0;
+    }
+ 
     return 0;
 }
