4e0d7e49b0383dc4fba628098ff39a310d10ec01
bengHak <qkwl4678@naver.com>
2021-02-22 00:53:12 +0900

    9252

--- 9252/Main.java
+++ 9252/Main.java
@@ -17,24 +17,44 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = "";
+        int i, j;
 
         strArr[0] = br.readLine();
         strArr[1] = br.readLine();
         len[0] = strArr[0].length();
         len[1] = strArr[1].length();
 
-        for (int i = 1; i <= len[0]; i++) {
-            for (int j = 1; j <= len[1]; j++) {
+        for (i = 1; i <= len[0]; i++) {
+            for (j = 1; j <= len[1]; j++) {
                 if (strArr[0].charAt(i - 1) == strArr[1].charAt(j - 1)) {
                     arr[i][j] = arr[i - 1][j - 1] + 1;
-                    s = s.concat(Character.toString(strArr[0].charAt(i - 1)));
                 } else {
                     arr[i][j] = maxValue(i, j);
                 }
             }
         }
 
+        i = len[0];
+        j = len[1];
+        int max, L, U;
+        while (i > 0 && j > 0) {
+            max = arr[i][j];
+            L = arr[i - 1][j];
+            U = arr[i][j - 1];
+            if (max > L && max > U) {
+                s = Character.toString(strArr[0].charAt(i - 1)) + s;
+                i -= 1;
+                j -= 1;
+            } else if (max == L && max > U) {
+                i -= 1;
+            } else if (max > L && max == U) {
+                j -= 1;
+            } else if (max == L && max == U) {
+                j -= 1;
+            }
+        }
+
         System.out.println(arr[len[0]][len[1]]);
-        System.out.println(s);
+        System.out.print(s);
     }
 }
